01e495b063a00b8cf347007e7cf5c50b38012f36
mariasaadeh <114923492+mariasaadeh@users.noreply.github.com>
2022-10-05 12:40:04 +0200

    update jenkins

--- src/main/java/exo5/Helloworld.java
+++ src/main/java/exo5/Helloworld.java
@@ -3,7 +3,7 @@ package exo5;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method
 		System.out.println("Hello maven");
 	}
 
